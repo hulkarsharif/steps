89883c3c1dd0f45f35a86e6fa3645b81806323b0
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-05-08 14:13:51 -0400

    Create chilrden props for mesaages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ export default function App() {
     return (
         <div>
             <Steps />
+            <StepMessage step={1}>
+                <p>Pass in content</p>
+                <p>✌️</p>
+            </StepMessage>
+            <StepMessage step={2}>
+                <p>Pass in content</p>
+                <p>✌️</p>
+            </StepMessage>
             {/* <Steps /> */}
         </div>
     );
@@ -39,10 +47,20 @@ function Steps() {
                         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
                     </div>
 
-                    <p className="message">
-                        Step{step}:{messages[step - 1]}
-                    </p>
-
+                    <StepMessage step={step}>
+                        {messages[step - 1]}
+                        <div className="buttons">
+                            <Button
+                                bgColor="#e7e7e7"
+                                textColor="#333"
+                                onClick={() =>
+                                    alert(`Learn how to ${messages[step - 1]}`)
+                                }
+                            >
+                                Learn how{" "}
+                            </Button>
+                        </div>
+                    </StepMessage>
                     <div className="buttons">
                         <Button
                             bgColor="#7950f2"
@@ -68,6 +86,15 @@ function Steps() {
     );
 }
 
+function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step{step}</h3>
+
+            {children}
+        </div>
+    );
+}
 function Button({ textColor, bgColor, onClick, children }) {
     return (
         <button
